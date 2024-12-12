79a7c090da5696a497d6ad5ecb142785d2e5b0af
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-12 14:10:27 +0500

    Initial Commit

---
+++ CoreDataPractice(ToDo)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataPractice(ToDo)
+//
+//  Created by Elexoft on 12/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
