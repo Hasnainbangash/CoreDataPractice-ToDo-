24db8c56809caab19fa0bc26fc39d946121cd78e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-12 15:03:19 +0500

    Databse data only entered

--- CoreDataPractice(ToDo)/ViewController.swift
+++ CoreDataPractice(ToDo)/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var item: [Any]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
