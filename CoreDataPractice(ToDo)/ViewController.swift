cbe26a4c082a7e394faa7187af617cced935e45d
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-12 15:43:47 +0500

    Add button work done

--- CoreDataPractice(ToDo)/ViewController.swift
+++ CoreDataPractice(ToDo)/ViewController.swift
@@ -43,6 +43,41 @@ class ViewController: UIViewController {
     
     @IBAction func addTapped(_ sender: Any) {
         
+        let alert = UIAlertController(title: "Add Task", message: "What's your Task", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter your Task"
+        }
+        
+        let submitButton = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            // TODO: Get the textfield for the alert
+            let textField = alert.textFields![0]
+            
+            // TODO: Create a person object
+            let newToDo = ToDo(context: self.context)
+            newToDo.toDoText = textField.text
+            
+            // TODO: Save the data
+            do {
+                try self.context.save()
+            } catch {
+                
+            }
+            
+            // TODO: Re-fetch the data
+            self.fetchTasks()
+        }
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("Cancel Button is pressed")
+        }
+        
+        alert.addAction(submitButton)
+        alert.addAction(cancelButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
 }
@@ -50,13 +85,14 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return item?.count ?? 0
-        return 1
+        return item?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCellReuseableCell", for: indexPath) as! TaskCell
-        cell.toDoTextLabel.text = "Hello I am doing work"
+        // TODO: Get todo task from array and set the label
+        let toDo = self.item![indexPath.row]
+        cell.toDoTextLabel.text = toDo.toDoText
         return cell
     }
 
